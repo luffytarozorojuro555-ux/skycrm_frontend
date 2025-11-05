1c602547f88ef38a19fe96a9b280947825f4a3dd
CHEVULA SOMESH <146305649+Somesh2504@users.noreply.github.com>
2025-11-05 13:53:18 +0530

    Back Button

--- src/components/ManageUsers.jsx
+++ src/components/ManageUsers.jsx
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../services/api";
 import { useState, useEffect } from "react";
 import UsersTable from "../components/UsersTable";
-
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 const ManageUsers = () => {
   const [selectedRoleId, setSelectedRoleId] = useState(null);
   const [usersData, setUsersData] = useState([]);
-
+  const navigate = useNavigate();
   const rolesQuery = useQuery({
     queryKey: ["roles"],
     queryFn: async () => (await api.get("/roles")).data,
@@ -38,6 +39,20 @@ const ManageUsers = () => {
 
   return (
     <div className="flex flex-col items-center  py-12 space-y-6">
+      {/* Back Button */}
+      <div className="w-full max-w-4xl flex justify-start mb-2 px-4">
+        <button
+          onClick={() => navigate("/admin")}
+          className="flex items-center gap-2 px-3 py-2 rounded-lg 
+                     text-sm font-medium text-gray-700 bg-gray-100 
+                     hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-300 
+                     dark:hover:bg-gray-700 transition"
+        >
+          <ArrowLeft className="w-4 h-4" />
+          Back
+        </button>
+      </div>
+
       <h1 className="text-2xl font-bold">Manage Users</h1>
 
       <div className=" p-4 rounded-2xl  w-full max-w-3xl">
